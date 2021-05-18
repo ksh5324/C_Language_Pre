9b974231b0dcd0628870fe851769d4478729f96c
ksh5324 <yolly050101@naver.com>
2021-05-18 14:04:07 +0900

    Create atoi_function.cpp

---
+++ atoi_function.cpp
@@ -0,0 +1,12 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main() {
+//	int i;
+//	char str[256];
+//	printf("Enter a number: ");
+//	//fgets(str,256,stdin);
+//	gets_s(str, 256);
+//	i = atoi(str);
+//	printf("입력된 정수값 :%d, 2배의 값: %d.\n", i, i * 2);
+//	return 0;
+//}
