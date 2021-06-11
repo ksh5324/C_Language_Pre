0d3fed795c0784f716a3e78d05dafe2a19edd3d7
ksh5324 <yolly050101@naver.com>
2021-06-11 21:03:44 +0900

    잔액 확인+돈 넣기

--- 은행.cpp
+++ 은행.cpp
@@ -4,12 +4,17 @@
 #include<stdlib.h>
 #include<time.h>
 
+FILE* fp[20];
+
+FILE* fpp[10];
+
+
 struct bankBook
 {
-	char userName[10];
+	char Name[10];
 	char id[30];
 	char password[30];
-	double money;
+	int money = 0;
 	FILE* fp;
 };
 
@@ -25,20 +30,19 @@ struct people
 typedef struct bank
 {
 	struct bankBook bank[100];
-	struct people people[20];
+	struct people people[10];
 	int i = 0, j = 0;
 }bank;
 
 // 은행 아이디 만들기 (O)
-// 통장 만들기
-// 돈 넣기
+// 통장 만들기 (0)
+// 돈 넣기 
 // 돈 보내기
 // 돈 꺼내기
-// 로그인
 // 액수 확인
 // 비번 바꾸기
 // 통장 없애기
-// 기록 확인
+// 기록 확인 (X)
 
 bank bank1;
 
@@ -81,7 +85,7 @@ re:
 
 
 
-void makeBankBook() { // 통장 생성
+int makeBankBook() { // 통장 생성
 	int searchBankBook;
 	int searchBankBookPassword;
 	int num[10];
@@ -105,6 +109,9 @@ void makeBankBook() { // 통장 생성
 		if (searchBankBookPassword <= bank1.j) {
 			printf("%s님 반갑습니다\n", bank1.people[bank1.j].userName);
 		}
+		else {
+			return 0;
+		}
 	}
 	printf("계좌번호는: ");
 	for (int zz = 0; zz < 10; zz++) {
@@ -112,9 +119,18 @@ void makeBankBook() { // 통장 생성
 		printf("%d", bank1.bank[bank1.i].id[zz]);
 	}
 	printf("\n");
-
-
-	/*printf("\n");
+	printf("통장이름: ");
+	scanf("%s", bank1.bank[bank1.i].Name);
+re:
+	printf("비밀번호: ");
+	for (int k = 0;; k++) {
+		bank1.bank[bank1.i].password[k] = getch();
+		if (bank1.bank[bank1.i].password[k] == 13) {
+			break;
+		}
+		printf("*");
+	}
+	printf("\n");
 	char bankEx[30] = { 0, };
 	printf("한번 더 입력해 주세요: ");
 	for (int k = 0;; k++) {
@@ -128,14 +144,183 @@ void makeBankBook() { // 통장 생성
 		printf("\n");
 		printf("틀렸습니다\n비밀번호를 다시 설정해주세요\n");
 		goto re;
-	}*/
+	}
+	bank1.i++;
+	printf("\n통장이 생성되었습니다\n");
+	return 0;
+}
+
+int checkMoney() { // 액수 확인
+	int searchBankBook;
+	int searchBankBookPassword;
+	int searchBankBookPassword2;
+	int num[10];
+	char pass2[30] = { 0, };
+	printf("\n----------액수 확인하기-----------\n");
+	char userId[30] = { 0, };
+	char pass[30] = { 0, };
+	char Name[30] = { 0, };
+	int bankBook;
+	printf("아이디: ");
+	scanf("%s", userId);
+	for (searchBankBook = 0; strcmp(bank1.people[bank1.j].id, userId) != 0 && searchBankBook <= bank1.j; searchBankBook++);
+	if (searchBankBook <= bank1.j) {
+		printf("비밀번호: ");
+		for (int k = 0;; k++) {
+			pass[k] = getch();
+			if (pass[k] == 13) {
+				break;
+			}
+			printf("*");
+		}
+		printf("\n");
+		if (strcmp(bank1.people[searchBankBook].password, pass) == 0) {
+			printf("%s님 반갑습니다\n", bank1.people[searchBankBook].userName);
+		}
+		else {
+			printf("비밀번호가 틀렸습니다\n");
+			return 0;
+		}
+	}
+	else {
+		printf("없는 아이디입니다\n");
+		return 0;
+	}
+	printf("통장을 선택해주세요: ");
+	scanf("%s", Name);
+	for (bankBook = 0; strcmp(bank1.bank[bankBook].Name, Name) != 0 && bankBook <= bank1.i; bankBook++);
+	if (bankBook <= bank1.i) {
+		printf("비밀번호: ");
+		for (int k = 0;; k++) {
+			pass2[k] = getch();
+			if (pass2[k] == 13) {
+				break;
+			}
+			printf("*");
+		}
+		if (strcmp(pass2, bank1.bank[bankBook].password) == 0) {
+			printf("\n잔액: ");
+			printf("%d", bank1.bank[bankBook].money);
+		}
+		else {
+			printf("비밀번호가 틀렸습니다\n");
+		}
+		
+	}
+	else {
+		printf("없는 통장입니다\n");
+	}
+	return 0;
+}
+
+int putMoney() {
+	int searchBankBook;
+	int searchBankBookPassword, money = 0;
+	int num[10];
+	printf("\n----------돈 넣기-----------\n");
+	char pass2[30] = { 0, };
+	char userId[30] = { 0, };
+	char pass[30] = { 0, };
+	char Name[30] = { 0, };
+	int bankBook;
+	printf("아이디: ");
+	scanf("%s", userId);
+	for (searchBankBook = 0; strcmp(bank1.people[bank1.j].id, userId) != 0 && searchBankBook <= bank1.j; searchBankBook++);
+	if (searchBankBook <= bank1.j) {
+		printf("비밀번호: ");
+		for (int k = 0;; k++) {
+			pass[k] = getch();
+			if (pass[k] == 13) {
+				break;
+			}
+			printf("*");
+		}
+		printf("\n");
+		if (strcmp(bank1.people[searchBankBook].password, pass) == 0) {
+			printf("%s님 반갑습니다\n", bank1.people[searchBankBook].userName);
+		}
+		else {
+			printf("비밀번호가 틀렸습니다\n");
+			return 0;
+		}
+	}
+	else {
+		printf("없는 아이디입니다\n");
+		return 0;
+	}
+	printf("통장을 선택해주세요: ");
+	scanf("%s", Name);
+	for (bankBook = 0; strcmp(bank1.bank[bankBook].Name, Name) != 0 && bankBook <= bank1.i; bankBook++);
+	if (bankBook <= bank1.i) {
+		printf("비밀번호: ");
+		for (int k = 0;; k++) {
+			pass2[k] = getch();
+			if (pass2[k] == 13) {
+				break;
+			}
+			printf("*");
+		}
+		if (strcmp(pass2, bank1.bank[bankBook].password) == 0) {
+			printf("\n넣을 금액: ");
+			int result;
+			scanf("%d", &result);
+			bank1.bank[bankBook].money += result;
+			printf("%d원\n", bank1.bank[bankBook].money);
+			printf("추가 되었습니다\n");
+		}
+		else {
+			printf("비밀번호가 틀렸습니다\n");
+		}
+
+	}
+	else {
+		printf("없는 통장입니다\n");
+	}
+	return 0;
+
+}
+
+void makeFile() {
+	fp[0] = fopen("통장1.txt", "w");
+	fp[1] = fopen("통장2.txt", "w");
+	fp[2] = fopen("통장3.txt", "w");
+	fp[3] = fopen("통장4.txt", "w");
+	fp[4] = fopen("통장5.txt", "w");
+	fp[5] = fopen("통장6.txt", "w");
+	fp[6] = fopen("통장7.txt", "w");
+	fp[7] = fopen("통장8.txt", "w");
+	fp[8] = fopen("통장9.txt", "w");
+	fp[9] = fopen("통장10.txt", "w");
+	fp[10] = fopen("통장11.txt", "w");
+	fp[11] = fopen("통장12.txt", "w");
+	fp[12] = fopen("통장13.txt", "w");
+	fp[13] = fopen("통장14.txt", "w");
+	fp[14] = fopen("통장15.txt", "w");
+	fp[15] = fopen("통장16.txt", "w");
+	fp[16] = fopen("통장17.txt", "w");
+	fp[17] = fopen("통장18.txt", "w");
+	fp[18] = fopen("통장19.txt", "w");
+	fp[19] = fopen("통장20.txt", "w");
+
+	fpp[0] = fopen("아이디1.txt", "w");
+	fpp[1] = fopen("아이디2.txt", "w");
+	fpp[2] = fopen("아이디3.txt", "w");
+	fpp[3] = fopen("아이디4.txt", "w");
+	fpp[4] = fopen("아이디5.txt", "w");
+	fpp[5] = fopen("아이디6.txt", "w");
+	fpp[6] = fopen("아이디7.txt", "w");
+	fpp[7] = fopen("아이디8.txt", "w");
+	fpp[8] = fopen("아이디9.txt", "w");
+	fpp[9] = fopen("아이디10.txt", "w");
 }
 
 int main() {
+	makeFile();
 	srand(time(NULL));
 	makeBankID();
 	makeBankBook();
-
+	putMoney();
+	checkMoney();
 
 	return 0;
 }
